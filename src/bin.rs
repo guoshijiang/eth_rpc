c05df770592eb961129942304b917d6997c31769
guoshijiang <guoshijiang2012@163.com>
2022-06-06 08:04:30 +0800

    ETH RPC 模拟实现

---
+++ src/bin.rs
@@ -0,0 +1,11 @@
+use ethock_lib::server;
+
+fn main() {
+    let mut addr: &str = "localhost:8545";
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 {
+        addr = &args[1][..];
+    }
+    println!("{}", addr);
+    server::Entry::new(addr, "debug").serve();
+}
